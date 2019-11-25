709fa8a870a3295f139b6393e39119e3ecde5ae5
luciferankon <44019225+luciferankon@users.noreply.github.com>
2019-11-25 15:57:30 +0530

    Added addition deletion graph

--- fetchData.js
+++ fetchData.js
@@ -4,7 +4,7 @@ const generateReport = function(){
 			return res.json();
 		})
 		.then(internDetails => {
-			const vlSpec = {
+			const vlSpecForFrequency = {
 				$schema: "https://vega.github.io/schema/vega-lite/v4.json",
 				data: {
 					values: internDetails,
@@ -16,10 +16,42 @@ const generateReport = function(){
 				},
 			};
 
-			const div = document.createElement("div");
-			div.setAttribute("id", "reports");
+			let div = document.createElement("div");
+			div.setAttribute("id", "frequency_report");
 			document.body.append(div);
-			vegaEmbed("#reports", vlSpec);
+			vegaEmbed("#frequency_report", vlSpecForFrequency);
+
+			const vlSpecForModification = {
+				$schema: "https://vega.github.io/schema/vega-lite/v4.json",
+				width: 300,
+				data: {
+					values: internDetails,
+				},
+				mark: "bar",
+				encoding: {
+					facet: { field: "internName", type: "ordinal", columns: 4 },
+					x: {
+						field: "oid",
+						type: "nominal",
+						band: 0.8,
+						axis: null,
+					},
+					y: {
+						aggregate: "sum",
+						field: "value",
+						type: "quantitative",
+					},
+					color: {
+						field: "modification",
+						type: "nominal",
+					},
+				},
+			};
+
+			div = document.createElement("div");
+			div.setAttribute("id", "modification");
+			document.body.append(div);
+			vegaEmbed("#modification", vlSpecForModification);
 		})
 		.catch(e => console.log(e));
 }
