835630a38994df66edcf7863959a48fbb0ef678b
Abhishek Gupta <abhishkg@thoughtworks.com>
2019-11-26 14:46:53 +0530

    Update the chart to display all the modification and deletions in the stacked bar chart.

--- fetchData.js
+++ fetchData.js
@@ -1,61 +1,74 @@
-const generateReport = function(){
-  fetch("https://commiters-vega.herokuapp.com/")
-		.then(res => {
-			return res.json();
-		})
-		.then(internDetails => {
-			const vlSpecForFrequency = {
-				$schema: "https://vega.github.io/schema/vega-lite/v4.json",
-				width: 300,
-				data: {
-					values: internDetails,
-				},
-				mark: "tick",
-				encoding: {
-					facet: { field: "internName", type: "ordinal", columns: 4 },
-					x: { field: "authoredDate", type: "temporal" },
-				},
-			};
+const mapper = function(internDetails) {
+  const additionDeltions = internDetails.map(x => {
+    x['del'] = x.modification == 'deletions' ? x.value * -1 : 0;
+    x['add'] = x.modification == 'additions' ? x.value : 0;
+    return x;
+  });
 
-			let div = document.createElement("div");
-			div.setAttribute("id", "frequency_report");
-			document.body.append(div);
-			vegaEmbed("#frequency_report", vlSpecForFrequency);
+  return additionDeltions.filter(x => {
+    const authoredDate = new Date(x.authoredDate);
+    return authoredDate >= new Date('2019-11-23T05:50:28.267Z');
+  });
+};
 
-			const vlSpecForModification = {
-				$schema: "https://vega.github.io/schema/vega-lite/v4.json",
-				width: 300,
-				data: {
-					values: internDetails,
-				},
-				mark: "bar",
-				encoding: {
-					facet: { field: "internName", type: "ordinal", columns: 4 },
-					x: {
-						field: "oid",
-						type: "nominal",
-						band: 0.8,
-						axis: null,
-					},
-					y: {
-						aggregate: "sum",
-						field: "value",
-						type: "quantitative",
-					},
-					color: {
-						field: "modification",
-						type: "nominal",
-					},
-				},
-			};
+const generateReport = function() {
+  fetch('https://commiters-vega.herokuapp.com/')
+    .then(res => {
+      return res.json();
+    })
+    .then(internDetails => {
+      const vlSpecForModification = {
+        $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
+        columns: 6,
+        data: {
+          values: mapper(internDetails)
+        },
+        facet: {
+          field: 'internName',
+          type: 'nominal'
+        },
+        spec: {
+          layer: [
+            {
+              mark: 'bar',
+              encoding: {
+                x: {
+                  field: 'authoredDate',
+                  timeUnit: 'monthsdatehoursminutes',
+                  type: 'ordinal',
+                  title: 'date',
+                  sort: 'null'
+                },
+                y: { field: 'add', type: 'quantitative' },
+                color: { value: 'green' }
+              }
+            },
+            {
+              mark: 'bar',
+              encoding: {
+                x: {
+                  field: 'authoredDate',
+                  timeUnit: 'monthsdatehoursminutes',
+                  type: 'ordinal',
+                  title: 'date',
+                  sort: ['authoredDate']
+                },
+                y: { field: 'del', type: 'quantitative' },
+                color: { value: 'red' }
+              }
+            }
+          ]
+        },
+        resolve: { scale: { x: 'independent', y: 'independent' } }
+      };
 
-			div = document.createElement("div");
-			div.setAttribute("id", "modification");
-			document.body.append(div);
-			vegaEmbed("#modification", vlSpecForModification);
-		})
-		.catch(e => console.log(e));
-}
+      div = document.createElement('div');
+      div.setAttribute('id', 'modification');
+      document.body.append(div);
+      vegaEmbed('#modification', vlSpecForModification);
+    })
+    .catch(e => console.log(e));
+};
 
 window.onload = generateReport;
 setInterval(generateReport, 60000);
